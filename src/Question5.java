7dab6bf58cd47f481f18fa9e68cc4d0c068804f3
Gareth Zuma <garethzuma28@gmail.com>
2026-03-19 05:36:59 +0200

    Prac1 Que4 GitHub update2

--- src/Question5.java
+++ src/Question5.java
@@ -1,2 +1,91 @@
-public class Question5 {
+import java.util.Scanner;
+
+public class BankAccount {
+    
+    
+    public String accountNumber;
+    public String ownerName;
+    public double balance;
+    
+
+    public BankAccount() {
+        accountNumber = "00000000";
+        ownerName = "Unknown";
+        balance = 0.0;
+    }
+    
+  
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    
+    public String getOwnerName() {
+        return ownerName;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+    
+  
+    public void setAccountNumber(String number) {
+        accountNumber = number;
+    }
+    
+    public void setOwnerName(String name) {
+        ownerName = name;
+    }
+    
+    public void setBalance(double amount) {
+        balance = amount;
+    }
+    
+  
+    public void deductMonthlyFee() {
+        balance = balance - 8.00;
+    }
+    
+   
+    public static void explainAccountPolicy() {
+        System.out.println("A monthly service fee of ZAR 8.00 will be deducted from your account.");
+    }
+    
+
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+   
+        BankAccount account = new BankAccount();
+        
+
+        System.out.print("Enter account number: ");
+        String accNum = keyboard.nextLine();
+        account.setAccountNumber(accNum);
+        
+        System.out.print("Enter account owner's name: ");
+        String name = keyboard.nextLine();
+        account.setOwnerName(name);
+        
+        System.out.print("Enter initial balance (ZAR): ");
+        double initialBalance = keyboard.nextDouble();
+        account.setBalance(initialBalance);
+        
+      
+        System.out.println("\n--- Account Details ---");
+        System.out.println("Account Number: " + account.getAccountNumber());
+        System.out.println("Owner: " + account.getOwnerName());
+        System.out.println("Balance before fee: ZAR " + account.getBalance());
+        
+   
+        explainAccountPolicy();
+        
+
+        account.deductMonthlyFee();
+        
+      
+        System.out.println("Balance after monthly fee: ZAR " + account.getBalance());
+        
+        keyboard.close();
+    }
 }
