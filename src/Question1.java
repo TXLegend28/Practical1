ea2db5ec9ba1afeb0466b746b993dfe52386d5f7
TXLegend28 <garethzuma28@gmail.com>
2026-03-19 13:39:48 +0200

    Prac1 Que5 IntelliJ update3

--- src/Question1.java
+++ src/Question1.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         double num1 = 2.0;
         double num2 = 5.0;
+        System.out.println("Enter your first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Enter your second number: ");
+        num2 = scanner.nextDouble();
+
 
         double percent = (num1 / num2) * 100;
         System.out.println(num1 + " is " + percent + "% of " + num2);
